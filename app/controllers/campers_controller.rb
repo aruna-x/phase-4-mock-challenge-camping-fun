4d1f6d65294ad6c26d99e83d9546106a88baca6e
Aruna <aruna.evan15@gmail.com>
2021-11-30 18:57:53 -0800

    commit tree 871e4821e4ce9ddd56527e2ec2afb0221c88c957

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,28 @@
+class CampersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        campers = Camper.all
+        render json: campers, status: :ok
+    end
+
+    def show
+        camper = Camper.find(params[:id])
+        render json: camper, include: :activities, status: :ok
+    end
+
+    def create
+        newCamper = Camper.create!(camper_params)
+        render json: newCamper, status: :created
+    end
+
+    private
+
+    def camper_params
+        params.permit(:name, :age)
+    end
+
+    def render_not_found_response
+        render json: {error: "Camper not found"}, status: :not_found
+    end
+end
